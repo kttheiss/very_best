2fbb75fb16037753b2c8cf26e8ea061845c35a39
anonymous anonymous <katherinetheiss@gmail.com>
2022-02-23 15:50:20 +0000

    Direct association between venues and bookmarks

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,6 +1,9 @@
 class Venue < ApplicationRecord
   # Direct associations
 
+  has_many   :bookmarks,
+             :dependent => :destroy
+
   has_many   :dishes_at_venues,
              :class_name => "UniqueListing",
              :dependent => :destroy
--- spec/models/venue_spec.rb
+++ spec/models/venue_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe Venue, type: :model do
   
     describe "Direct Associations" do
 
+    it { should have_many(:bookmarks) }
+
     it { should have_many(:dishes_at_venues) }
 
     end
